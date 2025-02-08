018a090803b72ca0f35bb2f7f4af9ae96e14bf11
gitkachoda <kumarsonuravi06@gmail.com>
2025-02-09 04:08:42 +0530

    casda

--- proxychor.py
+++ proxychor.py
@@ -165,5 +165,6 @@ def run_bot():
 
 # ✅ Run Flask & Bot in Parallel
 if __name__ == "__main__":
+    
     threading.Thread(target=run_bot, daemon=True).start()
-    app.run(host="0.0.0.0", port=PORT)  # ❌ Debug Mode Hata Diya
+    #app.run(host="0.0.0.0", port=PORT)  # ❌ Debug Mode Hata Diya
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "🚀 Starting Proxy Bot & Flask Server..."
-gunicorn -b 0.0.0.0:19237 proxychor:app &  # Flask ko background me run karega
-python3 proxychor.py  # Telegram bot ko start karega
+
+# ✅ Run the bot and Flask server with Gunicorn
+gunicorn -w 4 -b 0.0.0.0:$PORT proxychor:app
